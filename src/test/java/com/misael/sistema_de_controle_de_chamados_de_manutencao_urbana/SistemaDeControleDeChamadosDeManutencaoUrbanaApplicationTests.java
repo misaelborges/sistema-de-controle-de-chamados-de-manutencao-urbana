02d5e05f9c0a7644c55b255ca25e990891debabc
Mizael <mizaelborges2011@gmail.com>
2025-04-29 16:20:41 -0300

    adicionando a classe de teste que faltou ser adicionaod

---
+++ src/test/java/com/misael/sistema_de_controle_de_chamados_de_manutencao_urbana/SistemaDeControleDeChamadosDeManutencaoUrbanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misael.sistema_de_controle_de_chamados_de_manutencao_urbana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeControleDeChamadosDeManutencaoUrbanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
