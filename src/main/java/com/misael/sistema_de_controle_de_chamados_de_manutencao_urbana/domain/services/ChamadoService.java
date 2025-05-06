635da00b01edda57a19d38842968b959d50a5dee
Mizael <mizaelborges2011@gmail.com>
2025-05-06 14:38:26 -0300

    refatorando o try catch no metodo salvarChamado, para possiveis tratamento de exception

--- src/main/java/com/misael/sistema_de_controle_de_chamados_de_manutencao_urbana/domain/services/ChamadoService.java
+++ src/main/java/com/misael/sistema_de_controle_de_chamados_de_manutencao_urbana/domain/services/ChamadoService.java
@@ -1,7 +1,9 @@
 package com.misael.sistema_de_controle_de_chamados_de_manutencao_urbana.domain.services;
 
+import com.misael.sistema_de_controle_de_chamados_de_manutencao_urbana.domain.exceptions.ErroAoSalvarCliente;
 import com.misael.sistema_de_controle_de_chamados_de_manutencao_urbana.domain.models.Chamado;
 import com.misael.sistema_de_controle_de_chamados_de_manutencao_urbana.domain.repositories.IChamadoRepository;
+import jakarta.validation.UnexpectedTypeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,11 @@ public class ChamadoService {
     private IChamadoRepository chamadoRepository;
 
     public Chamado salvarChamado(Chamado chamado) {
-        return chamadoRepository.save(chamado);
+        try {
+            return chamadoRepository.save(chamado);
+        } catch (UnexpectedTypeException e) {
+            throw new ErroAoSalvarCliente(
+                    "Erro ao salvar o clinete: Verifique se os campos não estão em branco.", e.getCause());
+        }
     }
 }
